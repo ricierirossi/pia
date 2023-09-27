e680325dc0e67cd7853626d1bd1efa9033b43001
ricirossi <matheus.ricieri@hotmail.com>
2023-09-27 16:57:12 -0300

    Add backgroundImage to tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,9 @@ export default {
             borderRadius: {
                 20: "20px",
             },
+            backgroundImage: {
+                pia: "url('/assets/images/pia.png')",
+            },
         },
         plugins: [],
     },
