99fe684ae27efc97b579f6b152fd6a7d34a0f94c
ricirossi <matheus.ricieri@hotmail.com>
2023-09-28 17:33:14 -0300

    Change input tags
    Change input tags from dynamic to static and show/hide password function

--- pages/index.vue
+++ pages/index.vue
@@ -1,51 +1,85 @@
 <template>
-    <div class="h-screen font-lato md:flex">
+    <div class="h-screen font-lato tablet:flex">
         <div
-            class="h-40 w-full bg-pia bg-cover bg-top-2 md:h-auto md:w-1/2"
+            class="h-40 w-full bg-pia bg-cover bg-top-2 tablet:h-auto tablet:w-1/2"
         ></div>
         <div
-            class="text-base text-zinc-800 flex flex-col items-center md:w-1/2"
+            class="text-base text-zinc-800 flex flex-col items-center tablet:w-1/2"
         >
-            <h1 class="text-4xl my-5 md:mt-14">Venha fazer parte do Piá!</h1>
-            <div>
-                <div class="flex-col pb-8" v-for="(field, index) in formFields">
-                    <p class="pl-5">{{ formFields[index].label }}</p>
+            <h1 class="text-4xl my-5 tablet:mt-14">
+                Venha fazer parte do Piá!
+            </h1>
+            <div class="flex-col pb-8">
+                <div>
+                    <div class="pl-5">Digite seu e-mail</div>
                     <input
-                        :id="formFields[index].id"
-                        :placeholder="formFields[index].placeholder"
-                        :type="formFields[index].type"
-                        :minlength="formFields[index].minlength"
+                        id="email"
+                        type="email"
+                        placeholder="Ex: passarinho@gmail.com"
                         required
-                        class="text-gray-600 border rounded-20 top-2 border-gray-400 w-96 py-2 px-5"
+                        class="text-gray-600 border rounded-20 top-2 border-gray-400 w-96 py-2 px-5 mb-3"
                     />
-                    <div class="h-0" v-if="index > 1">
-                        <label
-                            :for="formFields[index].id"
-                            :index="index"
-                            v-if="!formFields[index].visibility"
-                            @click="
-                                formFields[index].visibility =
-                                    !formFields[index].visibility;
-                                showPassword(index);
-                            "
-                            class="material-symbols-outlined relative -top-8 left-88 text-gray-400 hover:cursor-pointer"
-                        >
-                            visibility
-                        </label>
-                        <label
-                            v-if="formFields[index].visibility"
-                            @click="
-                                formFields[index].visibility =
-                                    !formFields[index].visibility;
-                                showPassword(index);
-                            "
-                            class="material-symbols-outlined relative -top-8 left-88 text-gray-400 hover:cursor-pointer"
-                        >
-                            visibility_off
-                        </label>
+                </div>
+                <div>
+                    <div class="pl-5">Confirme seu e-mail</div>
+                    <input
+                        id="confirmEmail"
+                        type="email"
+                        placeholder="Ex: passarinho@gmail.com"
+                        required
+                        class="text-gray-600 border rounded-20 top-2 border-gray-400 w-96 py-2 px-5 mb-3"
+                    />
+                </div>
+                <div>
+                    <div class="pl-5">Digite sua senha</div>
+                    <input
+                        id="password"
+                        type="password"
+                        placeholder="Informe sua senha"
+                        required
+                        class="text-gray-600 border rounded-20 top-2 border-gray-400 w-96 py-2 px-5 mb-3"
+                    />
+                    <div
+                        v-if="visiblePassword"
+                        @click="showPassword()"
+                        class="material-symbols-outlined relative top-2 -left-9 text-gray-400 hover:cursor-pointer"
+                    >
+                        visibility_off
+                    </div>
+                    <div
+                        v-if="!visiblePassword"
+                        @click="showPassword()"
+                        class="material-symbols-outlined relative top-2 -left-9 text-gray-400 hover:cursor-pointer"
+                    >
+                        visibility
+                    </div>
+                </div>
+                <div>
+                    <div class="pl-5">Confirme sua senha</div>
+                    <input
+                        id="confirmPassword"
+                        type="password"
+                        placeholder="Confirme sua senha"
+                        required
+                        class="text-gray-600 border rounded-20 top-2 border-gray-400 w-96 py-2 px-5 mb-3"
+                    />
+                    <div
+                        v-if="visibleConfirmPassword"
+                        @click="showConfirmPassword()"
+                        class="material-symbols-outlined relative top-2 -left-9 text-gray-400 hover:cursor-pointer"
+                    >
+                        visibility_off
+                    </div>
+                    <div
+                        v-if="!visibleConfirmPassword"
+                        @click="showConfirmPassword()"
+                        class="material-symbols-outlined relative top-2 -left-9 text-gray-400 hover:cursor-pointer"
+                    >
+                        visibility
                     </div>
                 </div>
             </div>
+
             <div
                 class="text-xl text-white text-center bg-sky-600 w-96 py-3 px-20 rounded-20 hover:cursor-pointer"
             >
@@ -53,9 +87,9 @@
             </div>
             <p class="w-96 text-xs pt-2 pb-2 text-center">
                 Ao se inscrever, você concorda com os
-                <span>Termos de Serviço</span> e a
-                <span>Política de Privacidade</span>, incluindo o
-                <span>Uso de Cookies</span>.
+                <span class="underline">Termos de Serviço</span> e a
+                <span class="underline">Política de Privacidade</span>,
+                incluindo o <span class="underline">Uso de Cookies</span>.
             </p>
 
             <hr />
@@ -74,49 +108,23 @@
 export default {
     data: function () {
         return {
-            formFields: [
-                {
-                    id: "email",
-                    label: "Digite seu e-mail",
-                    placeholder: "Ex: passarinho@gmail.com",
-                    type: "email",
-                    minlength: null,
-                    visibility: null,
-                },
-                {
-                    id: "confirmEmail",
-                    label: "Confirme seu e-mail",
-                    placeholder: "Ex: passarinho@gmail.com",
-                    type: "email",
-                    minlength: null,
-                    visibility: null,
-                },
-                {
-                    id: "password",
-                    label: "Digite sua senha",
-                    placeholder: "Informe sua senha",
-                    type: "password",
-                    minlength: "8",
-                    visibility: false,
-                },
-                {
-                    id: "confirmPassword",
-                    label: "Confirme sua senha",
-                    placeholder: "Confirme sua senha",
-                    type: "password",
-                    minlength: "8",
-                    visibility: false,
-                },
-            ],
+            visiblePassword: false,
+            visibleConfirmPassword: false,
         };
     },
     methods: {
-        showPassword(index) {
-            if (this.formFields[index].type == "password") {
-                this.formFields[index].type = "text";
-            } else {
-                this.formFields[index].type = "password";
-            }
+        showPassword() {
+            this.visiblePassword = !this.visiblePassword;
+            this.visiblePassword
+                ? (document.getElementById("password").type = "text")
+                : (document.getElementById("password").type = "password");
+        },
+        showConfirmPassword() {
+            this.visibleConfirmPassword = !this.visibleConfirmPassword;
+            this.visibleConfirmPassword
+                ? (document.getElementById("confirmPassword").type = "text")
+                : (document.getElementById("confirmPassword").type =
+                      "password");
         },
     },
 };
