1174eb24f1284ef7a714ffc8614580f965005267
ricirossi <matheus.ricieri@hotmail.com>
2023-10-06 10:09:36 -0300

    Finish forms validation

--- components/CreateAccount.vue
+++ components/CreateAccount.vue
@@ -3,50 +3,83 @@
         <form class="flex-col justify-center items-center">
             <div class="pl-5">Digite seu e-mail</div>
             <input
+                @input="validatingForm"
+                id="email"
                 v-model="email"
                 type="email"
                 novalidate="true"
                 placeholder="Ex: passarinho@email.com"
-                class="text-gray-600 border rounded-20 top-2 border-gray-400 w-full py-2 px-5 mb-4 focus:outline-none"
-                :class="{ 'border-lime-600 border-2': isValidEmail }"
+                class="text-gray-600 border rounded-20 top-2 border-gray-400 w-full py-2 px-5 mb-2 focus:outline-none"
+                :class="{ 'border-lime-600 border-1': isValidEmail }"
             />
+            <div
+                v-if="!isValidEmail && validatingEmail"
+                class="h-5 text-xs text-red-600 pb-2 px-5"
+            >
+                Email inválido.
+            </div>
+            <div v-else class="h-5"></div>
             <div class="pl-5">Confirme seu e-mail</div>
             <input
+                @input="validatingForm"
+                id="confirmEmail"
                 v-model="confirmEmail"
-                type="confirmEmail"
+                type="email"
                 novalidate="true"
                 placeholder="Ex: passarinho@email.com"
-                class="text-gray-600 border rounded-20 top-2 border-gray-400 w-full py-2 px-5 mb-4 focus:outline-none"
-                :class="{ 'border-lime-600 border-2': equalEmail }"
+                class="text-gray-600 border rounded-20 top-2 border-gray-400 w-full py-2 px-5 mb-2 focus:outline-none"
+                :class="{ 'border-lime-600 border-1': equalEmail }"
             />
+            <div
+                v-if="!equalEmail && validatingConfirmEmail"
+                class="h-5 text-xs text-red-600 pb-2 px-5"
+            >
+                Os emails não são iguais.
+            </div>
+            <div v-else class="h-5"></div>
             <div class="pl-5">Digite sua senha</div>
             <input
+                @input="validatingForm"
+                id="password"
                 v-model="password"
                 type="password"
                 placeholder="Insira sua senha"
-                class="text-gray-600 border rounded-20 top-2 border-gray-400 w-full py-2 px-5 mb-4 focus:outline-none"
-                :class="{ 'border-lime-600 border-2': isValidPassword }"
+                class="text-gray-600 border rounded-20 top-2 border-gray-400 w-full py-2 px-5 mb-2 focus:outline-none"
+                :class="{ 'border-lime-600 border-1': isValidPassword }"
             />
+            <div
+                v-if="!isValidPassword && validatingPassword"
+                class="text-xs text-red-600 pb-2 px-5"
+            >
+                Senha deve conter: 8 caracteres, 1 letra minúscula, 1 letra
+                maiúscula, 1 número, 1 caracter especial.
+            </div>
+            <div v-else class="h-5"></div>
             <div class="pl-5">Confirme sua senha</div>
             <input
+                @input="validatingForm"
+                id="confirmPassword"
                 v-model="confirmPassword"
                 type="password"
                 placeholder="Confirme sua senha"
-                class="text-gray-600 border rounded-20 top-2 border-gray-400 w-full py-2 px-5 mb-1 focus:outline-none"
-                :class="{ 'border-lime-600 border-2': equalPassword }"
+                class="text-gray-600 border rounded-20 top-2 border-gray-400 w-full py-2 px-5 mb-2 focus:outline-none"
+                :class="{ 'border-lime-600 border-1': equalPassword }"
             />
-            <div class="text-xs text-gray-600 pb-2 px-5">
-                A senha deve incluir ao menos:
-                <ul class="px-6">
-                    <li>8 dígitos</li>
-                    <li>1 letra minúscula</li>
-                    <li>1 letra maiúscula</li>
-                    <li>1 dígito</li>
-                    <li>1 caractere especial</li>
-                </ul>
+            <div
+                v-if="!equalPassword && validatingConfirmPassword"
+                class="h-5 text-xs text-red-600 pb-2 px-5"
+            >
+                As senhas não são iguais.
             </div>
+            <div v-else class="h-5"></div>
+
             <button
-                class="text-xl text-white text-center bg-sky-600 w-full py-3 rounded-20 hover:cursor-pointer"
+                @click.prevent="submitForm"
+                class="text-xl text-white text-center bg-sky-600 w-full py-3 rounded-20 mt-2 hover:cursor-pointer"
+                :class="{
+                    unavailable: !available,
+                }"
+                :disabled="!available"
             >
                 Crie sua conta já
             </button>
@@ -61,6 +94,25 @@ const email = ref("");
 const confirmEmail = ref("");
 const password = ref("");
 const confirmPassword = ref("");
+
+const validatingEmail = ref(false);
+const validatingConfirmEmail = ref(false);
+const validatingPassword = ref(false);
+const validatingConfirmPassword = ref(false);
+
+function validatingForm(e) {
+    const inputID = e.target.id;
+
+    if (inputID == "email") {
+        validatingEmail.value = true;
+    } else if (inputID == "confirmEmail") {
+        validatingConfirmEmail.value = true;
+    } else if (inputID == "password") {
+        validatingPassword.value = true;
+    } else if (inputID == "confirmPassword") {
+        validatingConfirmPassword.value = true;
+    }
+}
 
 const isValidEmail = computed(() => {
     return /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email.value);
@@ -78,4 +130,17 @@ const equalPassword = computed(() => {
         password.value == confirmPassword.value && confirmPassword.value != ""
     );
 });
+const available = computed(() => {
+    return equalEmail.value && equalPassword.value;
+});
+
+const submitForm = computed(() => {
+    console.log("Criando sua conta Piá!");
+});
 </script>
+
+<style scoped>
+.unavailable {
+    background-color: rgb(186 230 253);
+}
+</style>
