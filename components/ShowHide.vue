b4308ae6f6a261cfe3bf7a5b73992fb5b6910914
ricirossi <matheus.ricieri@hotmail.com>
2023-10-07 15:45:02 -0300

    Create ShowHide.vue

---
+++ components/ShowHide.vue
@@ -0,0 +1,22 @@
+<template>
+    <div
+        v-if="showHide"
+        class="material-symbols-outlined absolute right-2 top-20 text-gray-400 hover:cursor-pointer"
+    >
+        visibility_off
+    </div>
+    <div
+        v-if="!showHide"
+        class="material-symbols-outlined absolute right-2 top-20 text-gray-400 hover:cursor-pointer"
+    >
+        visibility
+    </div>
+</template>
+
+<script>
+export default {
+    props: {
+        showHide: Boolean,
+    },
+};
+</script>
