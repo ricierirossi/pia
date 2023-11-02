e02b60d5cdf4c44d4ffc6f197c8fb6c4297ccf5a
Ricieri <matheus.ricieri@hotmail.com>
2023-11-02 14:29:58 -0300

    Delete all database related

--- db/app.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const User = require("./User");
-
-const port = 3001;
-const app = express();
-
-app.use(express.json());
-
-app.listen(port, console.log(`#####   Servidor rodando na porta ${port}`));
