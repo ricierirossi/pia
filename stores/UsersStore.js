94287d36c7494ecdb10dcfa0bf69b04702781bd9
Chopper <matheus.ricieri@hotmail.com>
2023-11-08 09:06:58 -0300

    Finish fetch data

--- stores/UsersStore.js
+++ stores/UsersStore.js
@@ -1,24 +1,18 @@
 import { defineStore } from "pinia";
-const User = require("./User");
 
 export const useUsersStore = defineStore("usersStore", {
     state: () => ({
-        users: [{ email: "teste1@teste1" }],
+        users: [],
     }),
 
     actions: {
-        /*async*/ createUser(user) {
-            console.log("Criando conta!");
-            return this.users.push(user);
-
-            // const res = await fetch("http://localhost:3001/users", {
-            //     method: "POST",
-            //     body: JSON.stringify(user),
-            //     headers: { "Content-Type": "application/json" },
-            // });
-            // if (res.error) {
-            //     console.log(res.error);
-            // }
+        async createUser(user) {
+            this.users.push(user);
+            await fetch("http://localhost:3001/users", {
+                method: "POST",
+                body: JSON.stringify(user),
+                headers: { "Content-Type": "application/json" },
+            });
         },
     },
 });
